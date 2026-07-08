ed91801da12c766ace15becbe69d665cb3ad7d21
isveckrali <mehmet.can.seyhan1@gmail.com>
2026-07-08 08:30:16 +0200

    Error page done

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FormInput = () => {
+  return (
+    <div className="form-control ">
+
+      <label className="label">
+        <span className="label-text">What is your name?</span>
+      </label>
+
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered"
+      />
+
+    </div>
+  );
+};
+
+
+export default FormInput
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useRouteError, Link } from 'react-router-dom';
 
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
+  const error = useRouteError();
+  console.log(error);
 
-export default Error
+  if (error?.status === 404) {
+    return (
+      <main className="grid min-h-[100vh] place-items-center px-8">
+        <div className="text-center">
+          <div className="text-9xl font-semibold text-primary">404</div>
+
+          <div className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page not found
+          </div>
+
+          <p className="mt-6 text-lg leading-7">
+            Sorry, we couldn&apos;t find the page you&apos;re looking for.
+          </p>
+
+          <div className="mt-10">
+            <Link to="/" className="btn btn-secondary">
+              Go Back Home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="grid min-h-[100vh] place-items-center px-8">
+      <h4 className="text-center font-bold text-4xl">
+        There was an error
+      </h4>
+    </main>
+  );
+};
+
+export default Error;
