a44e69df34eb1a68ca6319494bcb6c30759a947c
“isveckrali” <mehmet.can.seyhan1@gmail.com>
2026-07-27 11:35:07 +0200

    product detail page initialized

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import { customFetch, formatPrice } from '../utils'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 
 
-export const loader = async({params}) => {
+export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`)
 
-  return {product: response.data.data}
+  return { product: response.data.data }
 }
 
 const SingleProduct = () => {
-  const {product } = useLoaderData()
-  const {image, title, price, description, colors, company } = product.attributes 
+  const { product } = useLoaderData()
+  const { image, title, price, description, colors, company } = product.attributes
   const dollarsAmount = formatPrice(price)
   return (
-    <div>SingleProduct</div>
+    <section>
+      <div>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/products"> Products </Link>
+          </li>
+        </ul>
+      </div>
+      {/* PRODUCT */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* IMAGE */}
+        <img src={image} alt={image} className='w-96 object-cover rounded-lg lg:w-full' />
+        {/* PRODUCT */}
+        <div>
+          <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl text-neutral-content font-bold mt-2'>
+            {company}
+          </h4>
+          <p className='mt-3 text-xl'> {dollarsAmount} </p>
+          <p className='mt-6 leading-8'> {description}</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
