1f9107098dda16e01013a18c43c4e9e38e4d3ebe
“isveckrali” <mehmet.can.seyhan1@gmail.com>
2026-08-21 14:00:44 +0200

    theme setting was changed/refactored

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,26 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+};
+
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('user')) || null;
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem('theme') || themes.winter;
+  document.documentElement.setAttribute('data-theme', theme);
+  return theme;
+};
 
 const initialState = {
-    user: { username: "coding addict" },
-    theme: "dracula",
-}
+  user: getUserFromLocalStorage(),
+  theme: getThemeFromLocalStorage(),
+};
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        loginUser: (state, action) => {
-            console.log("login")
-        },
-        loginUser: (state) => {
-            console.log("logout")
-        },
-        toggleTheme: (state) => {
-            console.log("toggle theme")
-        }
-    }
-})
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      const user = { ...action.payload.user, token: action.payload.jwt };
+      state.user = user;
+      localStorage.setItem('user', JSON.stringify(user));
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      localStorage.removeItem('user');
+      toast.success('Logged out successfully');
+    },
+    toggleTheme: (state) => {
+      const { dracula, winter } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute('data-theme', state.theme);
+      localStorage.setItem('theme', state.theme);
+    },
+  },
+});
 
-export const {loginUser, logoutUser, toggleTheme} = userSlice.actions
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
